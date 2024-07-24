90c781ac345751e658607eee17afe051678e1c23
danofoundation <dan.dev947366@gmail.com>
2024-07-24 08:11:08 +0300

    Update main

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 import time 
+
+def calculate_sum_square(n):   
+    sum_square = 0
+    for i in range(n):
+        sum_square += i **2
+        
+    print(sum_square)
+    
+def sleep_a_little(seconds):
+    time.sleep(seconds)
+    
+def main():
+    calc_start_time = time.time()
+    for i in range(5):
+        calculate_sum_square(i)
+        
+    print('Calculating sum of square took: ', round(time.time() - calc_start_time))
+    
+    sleep_start_time = time.time()
+    for i in range(1,6):
+        sleep_a_little(i)
+    print('Sleeping time took: ', round(time.time() - sleep_start_time))
+if __name__ == "__main__":
+    main()
