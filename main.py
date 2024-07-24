a7d1b57df8760e0f9d787b58c23f254bc1bcc685
danofoundation <dan.dev947366@gmail.com>
2024-07-24 08:26:27 +0300

    Add threading

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import time 
-
+import threading
 def calculate_sum_square(n):   
     sum_square = 0
     for i in range(n):
@@ -12,14 +12,31 @@ def sleep_a_little(seconds):
     
 def main():
     calc_start_time = time.time()
+    current_threads = []
     for i in range(5):
-        calculate_sum_square(i)
+        maximum_value = (i+1) *1000000
+        t = threading.Thread(target=calculate_sum_square, args=(maximum_value, ))
+        #calculate_sum_square(maximum_value)
+        t.start()
+        current_threads.append(t)
         
-    print('Calculating sum of square took: ', round(time.time() - calc_start_time))
+    for i in range(len(current_threads)):
+        current_threads[i].join()
+        
+    print('Calculating sum of square took: ', round(time.time() - calc_start_time, 3))
     
     sleep_start_time = time.time()
-    for i in range(1,6):
-        sleep_a_little(i)
-    print('Sleeping time took: ', round(time.time() - sleep_start_time))
+    
+    for seconds in range(1,6):
+        t = threading.Thread(target=sleep_a_little, args=(seconds,))
+        #sleep_a_little(i)
+        t.start()
+        current_threads.append(t)
+    for i in range(len(current_threads)):
+        current_threads[i].join()
+        
+    print('Sleep took: ', round(time.time() - sleep_start_time, 3))
+    
+    
 if __name__ == "__main__":
     main()
