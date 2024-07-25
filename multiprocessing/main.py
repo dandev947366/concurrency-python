e2867c08cc6fbfb8dd877d8a6966ac262b2c842c
danofoundation <dan.dev947366@gmail.com>
2024-07-25 08:23:36 +0300

    Update main

--- multiprocessing/main.py
+++ multiprocessing/main.py
@@ -1,21 +1,26 @@
 import time
-from multiprocessing import Process
+from multiprocessing import Process, Queue
 
-
-
-def sum_of_squares(n):
-    total = 0
-    for i in range(n):
-        total += i * i
+def check_value_in_list(x, i, num_of_processes, queue):
+    max_num_to_check = 10**8
+    lower = int(i * max_num_to_check / num_of_processes)
+    upper = int((i + 1) * max_num_to_check / num_of_processes)
+    number_of_hits = 0
+    for j in range(lower, upper):
+        if j in x:
+            number_of_hits += 1
+    queue.put((lower, upper, number_of_hits))
 
 if __name__ == '__main__':
     num_processes = 4
-    n = 10**7
+    comparision_list = [1, 2, 3]
+    comparision_set = set(comparision_list)  # Using a set for faster lookup
+    queue = Queue()
 
     start_time = time.time()
     processes = []
     for i in range(num_processes):
-        t = Process(target=sum_of_squares, args=(n,))
+        t = Process(target=check_value_in_list, args=(comparision_set, i, num_processes, queue))
         processes.append(t)
 
     for t in processes:
@@ -23,5 +28,14 @@ if __name__ == '__main__':
 
     for t in processes:
         t.join()
+
+    queue.put('DONE')
+
+    while True:
+        v = queue.get()
+        if v == 'DONE':
+            break
+        lower, upper, number_of_hits = v
+        print("Between", lower, "and", upper, "we have", number_of_hits)
 
     print("Everything took:", time.time() - start_time, "seconds")
